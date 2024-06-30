fde602576a78c57c6d70c3cb386dd0e98dbdb604
Dragos0129 <dragosbaba@gmail.com>
2024-06-30 17:41:48 +0300

    commit 8

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,11 @@ import user from '../data/user.json';
 import Profile from './Profile/Profile';
 import data from '../data/data.json';
 import Statistics from './Statistics/Statistics';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
+import friends from '../data/friends.json';
+import FriendList from './FriendList/FriendList';
+
 export default function App() {
   return (
     <div>
@@ -14,6 +19,8 @@ export default function App() {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
